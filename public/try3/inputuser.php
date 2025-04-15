f4b339e183435990591d1d3b71e1b490ac428617
yamada1023 <141080366+yamada1023@users.noreply.github.com>
2025-04-15 01:41:09 +0000

    fixed: try3

--- public/try3/inputuser.php
+++ public/try3/inputuser.php
@@ -9,10 +9,31 @@
     <div class="login-box">
       <h2>
 <?php
+// 値の存在チェック
+if (empty($_POST['username']) || empty($_POST['useraddress']) || empty($_POST['usermail'])) {
+  die('値を入力してください');
+}
+
+// 名前：20文字までの日本語入力
+if (!mb_ereg('^[ぁ-んァ-ン一-龥]{1,20}$', $_POST['username'])) {
+  die('有効な名前を入力してください');
+}
+
+// 住所：50文字までの日本語入力
+if (!mb_ereg('^[ぁ-んァ-ン一-龥]{1,50}$', $_POST['useraddress'])) {
+  die('有効な住所を入力してください');
+}
+
+// メールアドレス：100文字まで, [.-_@]が利用可能
+if (!mb_ereg('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$', $_POST['usermail'])) {
+  die('有効なメールアドレスを入力してください');
+}
+
+// それぞれの値を取得
 echo "あなたが入力した値<br>";
-echo "名前：" . $_GET['username'] . "<br>";
-echo "住所：" . $_GET['useraddress']. "<br>";
-echo "メールアドレス：" . $_GET['usermail'];
+echo "名前：" . $_POST['username'] . "<br>";
+echo "住所：" . $_POST['useraddress']. "<br>";
+echo "メールアドレス：" . $_POST['usermail'];
 ?>
     </h2>
     </div>
