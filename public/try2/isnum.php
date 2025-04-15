5266097046641528ba0f3afe8b12e58e81184eb5
yamada1023 <141080366+yamada1023@users.noreply.github.com>
2025-04-15 01:19:54 +0000

    fix: try2

--- public/try2/isnum.php
+++ public/try2/isnum.php
@@ -9,9 +9,15 @@
     <div class="login-box">
       <h2>
 <?php
-
-
-echo "入力された数字は： " . $_GET['indata'];
+if (empty($_GET['indata'])) { // 入力欄に値が入っているか確認
+  die('値を入力してください');
+} else {
+  $data = filter_input(INPUT_GET, 'indata');  // 入力欄に無理やり配列が入っていないか確認
+  if (!mb_ereg('^[0-9]+$', $data)) {
+    die('数字を入力してください');  // 正規表現を使って文字をチェック
+  }
+  echo '入力された数字: '. $data;
+}
 ?>
     </h2>
     </div>
